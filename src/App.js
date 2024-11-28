744bf7604d8f10de6ba6940bf250568064819077
code malayalam <nikithajoemon@gmail.com>
2024-11-28 17:02:33 +0530

    changes1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./component/header/Header.jsx";
+import Nav from "./component/Navigation/Nav.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
