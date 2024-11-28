ed2a0ec655b370ae6f826bdc74b57683d432d372
code malayalam <nikithajoemon@gmail.com>
2024-11-28 17:18:45 +0530

    image section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import Nav from "./component/Navigation/Nav.jsx";
+import Body from "./component/Body/Body.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Body />
+    </div>
+  );
 }
 
 export default App;
