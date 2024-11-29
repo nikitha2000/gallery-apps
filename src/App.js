c513bc22a699383215586574c8cef21423887220
code malayalam <nikithajoemon@gmail.com>
2024-11-29 08:28:30 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Header from "./component/header/Header.jsx";
 import Nav from "./component/Navigation/Nav.jsx";
 import Body from "./component/Body/Body.jsx";
 
