d52dccd3ac3b89f1f8947b9e79687eb2ad7c8b6d
code malayalam <nikithajoemon@gmail.com>
2024-11-29 10:30:46 +0530

    hover

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import Header from "./component/header/Header.jsx";
+import Nav from "./component/Navigation/Nav.jsx";
+import Body from "./component/Body/Body.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Body />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/Body/Hover.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Hover.css";
+import Button from "../Button";
+
+const ActionButtons = ({ isHovered }) => {
+  return (
+    <div>
+      {isHovered && (
+        <div className="top-left-button">
+          <Button
+            label={<img src="/svgexport-16.svg" />}
+            className="save-button"
+          />
+          <Button
+            label={<img src="/svgexport-17.svg" />}
+            className="save-button"
+          />
+        </div>
+      )}
+      <div className="bottom-left">
+        <Button label="download" className="download-btn" />
+      </div>
+    </div>
+  );
+};
