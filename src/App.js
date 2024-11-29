6d2311d038d2a8b364301d67ea99b81035c35c64
code malayalam <nikithajoemon@gmail.com>
2024-11-29 11:05:51 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Header from "./component/header/Header.jsx";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
