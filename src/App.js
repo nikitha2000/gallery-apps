8f131f04bc4621516daac15ce1b30de3d9dbb073
code malayalam <nikithajoemon@gmail.com>
2024-12-05 10:41:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Favourites from "./pages/FavouritesPage.jsx";
+import Header from "./component/header/Header.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Photos from "./pages/Photos.jsx";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/photos" element={<Photos />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/Photos.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import GalleryLayout from "../component/Gallery/GalleryLayout";
+
+function Photos() {
+  return <GalleryLayout />;
+}
+
+export default Photos;
